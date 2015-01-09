32387656589c1d89db2276353b1d7d1e874d148a
mcgeld <m.c.geld@aggiemail.usu.edu>
2015-01-09 14:45:00 -0700

    New App
    Just made an app and I’m adding the files for that.

---
+++ proximityBattleExperiments/proximityBattleExperiments/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  proximityBattleExperiments
+//
+//  Created by Malcolm Geldmacher on 1/9/15.
+//  Copyright (c) 2015 Vient Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
